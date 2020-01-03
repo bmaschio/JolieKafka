f3a5ea4f5391679becf64c8c4432fc7884970a93
Balint Maschio <bmaschio77@gmail.com>
2020-01-03 15:22:24 +0100

    Added interface

--- JolieKafkaConnector/src/main/java/joliekafkaconnector/JolieConsumerCreator.java
+++ JolieKafkaConnector/src/main/java/joliekafkaconnector/JolieConsumerCreator.java
@@ -20,48 +20,62 @@ import org.apache.kafka.common.serialization.*;
  */
 public class JolieConsumerCreator {
 
-    public static Consumer<Object, Object> createConsumer(Value v) throws Exception {
+    public static Consumer<Object, Object> createConsumer(Value v, JolieKafkaTypeEnum keyType , JolieKafkaTypeEnum valueType) throws Exception {
         Properties props = new Properties();
 
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, v.getFirstChild("broker").strValue());
         props.put(ConsumerConfig.GROUP_ID_CONFIG, v.getFirstChild("groupId").strValue());
 
-        if (JolieKafkaTypeEnum.BYTES.type().equals(v.getFirstChild("keyType").strValue())) {
-            props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
-        } else if (JolieKafkaTypeEnum.INTEGER.type().equals(v.getFirstChild("keyType").strValue())) {
-            props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
-        } else if (JolieKafkaTypeEnum.DOUBLE.type().equals(v.getFirstChild("keyType").strValue())) {
-            props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, DoubleDeserializer.class.getName());
-        } else if (JolieKafkaTypeEnum.STRING.type().equals(v.getFirstChild("keyType").strValue())) {
-            props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        } else if (JolieKafkaTypeEnum.LONG.type().equals(v.getFirstChild("keyType").strValue())) {
-            props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        } else if (JolieKafkaTypeEnum.VALUE.type().equals(v.getFirstChild("keyType").strValue())) {
-            props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ValueJolieDeserializer.class.getName());
-        } else {
-            throw (new Exception());
+        switch (keyType) {
+            case STRING:
+                props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+                break;
+            case INTEGER:
+                props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
+                break;
+            case LONG:
+                props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+                break;
+            case VALUE:
+                props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ValueJolieSerializer.class.getName());
+                break;
+            case BYTES:
+                props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
+                break;
+            case DOUBLE:
+                props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, DoubleDeserializer.class.getName());
+                break;
+            default:
+                break;
         }
 
-        if (JolieKafkaTypeEnum.BYTES.type().equals(v.getFirstChild("valueType").strValue())) {
-            props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
-        } else if (JolieKafkaTypeEnum.INTEGER.type().equals(v.getFirstChild("valueType").strValue())) {
-            props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
-        } else if (JolieKafkaTypeEnum.DOUBLE.type().equals(v.getFirstChild("valueType").strValue())) {
-            props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, DoubleDeserializer.class.getName());
-        } else if (JolieKafkaTypeEnum.STRING.type().equals(v.getFirstChild("valueType").strValue())) {
-            props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        } else if (JolieKafkaTypeEnum.LONG.type().equals(v.getFirstChild("valueType").strValue())) {
-            props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        } else if (JolieKafkaTypeEnum.VALUE.type().equals(v.getFirstChild("valueType").strValue())) {
-            props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ValueJolieDeserializer.class.getName());
-        } else {
-            throw (new Exception());
+        switch (valueType) {
+            case STRING:
+                props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+                break;
+            case INTEGER:
+                props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
+                break;
+            case LONG:
+                props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+                break;
+            case VALUE:
+                props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ValueJolieSerializer.class.getName());
+                break;
+            case BYTES:
+                props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
+                break;
+            case DOUBLE:
+                props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, DoubleDeserializer.class.getName());
+                break;
+            default:
+                throw (new Exception());
         }
 
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"true");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, IKafkaConstants.OFFSET_RESET_EARLIER);
         Consumer<Object, Object> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Collections.singletonList(IKafkaConstants.TOPIC_NAME));
+        consumer.subscribe(Collections.singletonList( v.getFirstChild("topic").strValue()));
         return consumer;
     }
 }
--- JolieKafkaConnector/src/main/java/joliekafkaconnector/JolieKafkaService.java
+++ JolieKafkaConnector/src/main/java/joliekafkaconnector/JolieKafkaService.java
@@ -1,37 +1,83 @@
 package joliekafkaconnector;
 
+import jolie.net.CommMessage;
+import jolie.runtime.AndJarDeps;
 import jolie.runtime.JavaService;
 import jolie.runtime.Value;
 import jolie.runtime.embedding.RequestResponse;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.consumer.Consumer;
 
 import java.util.concurrent.ExecutionException;
 
 
+
+@AndJarDeps( { "jolie-js.jar", "kafka_2.13-2.4.0.jar"  } )
 public class JolieKafkaService extends JavaService {
 
     private  class ConsumerThread extends Thread {
         private boolean keepRun = true;
+        private Consumer<Object, Object> consumer;
+
 
         public void kill()
         {
             keepRun = false;
+            consumer.close();
             this.interrupt();
         }
 
         @Override
         public void run()
         {
+              while (keepRun){
+                  ConsumerRecords<Object, Object> consumerRecords = consumer.poll(1);
+                  consumerRecords.forEach( record -> {
+                      Value recordValue = Value.create();
+                      recordValue.getFirstChild("payload").getFirstChild("key").setValue(record.key());
+                      recordValue.getFirstChild("payload").getFirstChild("value").setValue(record.value());
+                      sendMessage( CommMessage.createRequest( "consumerIn", "/", recordValue) );
+                  });
 
+              }
         }
 
 
 
+        public void setValue (Value v) throws Exception {
+            JolieKafkaTypeEnum keyType = null ;
+            JolieKafkaTypeEnum valueType = null ;
+            if (v.getFirstChild("keyType").strValue().equals("long")){
+                keyType = JolieKafkaTypeEnum.LONG;
+            } else if (v.getFirstChild("keyType").strValue().equals("int")){
+                keyType = JolieKafkaTypeEnum.INTEGER;
+            } else if (v.getFirstChild("keyType").strValue().equals("string")){
+                keyType = JolieKafkaTypeEnum.STRING;
+            }
+
+            if (v.getFirstChild("valueType").strValue().equals("long")){
+                valueType = JolieKafkaTypeEnum.LONG;
+            } else if (v.getFirstChild("valueType").strValue().equals("int")){
+                valueType = JolieKafkaTypeEnum.INTEGER;
+            } else if (v.getFirstChild("valueType").strValue().equals("string")){
+                valueType = JolieKafkaTypeEnum.STRING;
+            }else if (v.getFirstChild("valueType").strValue().equals("double")){
+                valueType = JolieKafkaTypeEnum.DOUBLE;
+            } else if (v.getFirstChild("valueType").strValue().equals("value")){
+                valueType = JolieKafkaTypeEnum.VALUE;
+            }
+            consumer = JolieConsumerCreator.createConsumer(v , keyType ,valueType);
+        }
+
     }
+
+    private  ConsumerThread consumerThread;
+
     @RequestResponse
-    public Value setProducer(Value v){
+    public Value sendMessageToTopic(Value v){
 
         for( int index = 0; index < IKafkaConstants.MESSAGE_COUNT; index++ ) {
             Object key;
@@ -85,13 +131,24 @@ public class JolieKafkaService extends JavaService {
                 System.out.println( e );
             }
         }
-
-
         return Value.create();
     }
     @RequestResponse
-    public Value setConsumer(Value v){
+    public void setConsumer(Value v) throws Exception {
+       consumerThread = new ConsumerThread();
+       consumerThread.setValue(v);
+       consumerThread.start();
 
-            return Value.create();
+    }
+
+    @Override
+    protected void finalize()
+            throws Throwable
+    {
+        try {
+            consumerThread.kill();
+        } finally {
+            super.finalize();
+        }
     }
 }
